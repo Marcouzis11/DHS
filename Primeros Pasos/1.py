460fa4aacaa10026276a5b36241bc5d81f7c29ac
Marcos Agustin Garcia Mombello <mgarcia975@alumnos.iua.edu.ar>
2025-08-11 11:05:31 -0300

    asd

--- Primeros Pasos/1.py
+++ Primeros Pasos/1.py
@@ -1,1 +1,16 @@
-asdasds
+for nro in list(range(0,10)):
+    if nro == 5 :
+        break
+    print(nro)
+    
+print("Fin")
+
+try :
+    x = int(input("Ingrese entero: "))
+    print(x)
+except KeyboardInterrupt :
+    print("Detenido")
+except ValueError :
+    print("No se puede convertir")
+finally :
+    print("Fin")
